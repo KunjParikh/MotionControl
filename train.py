d62ef41c2537d972b11b4714d9664162a559e7d8
Kunj <kunjparikh6@gmail.com>
2020-04-07 07:54:41 -0700

    Update training script to handle multiple shapes, train LSTM one after another. Save model and scalar data generated after an experimental training run.

--- train.py
+++ train.py
@@ -1,7 +1,6 @@
 # LSTM for international airline passengers problem with regression framing
 import numpy as np
 import matplotlib.pyplot as plt
-from pandas import read_csv
 import math
 from keras.models import Sequential
 from keras.layers import Dense
@@ -12,10 +11,11 @@ import pickle
 from keras.callbacks import EarlyStopping
 from keras.callbacks import ModelCheckpoint
 import os
+import pandas as pd
 #Comment this to use GPU. But looks like for me CPU (~10s) is faster than GPU (~80s).
 os.environ["CUDA_VISIBLE_DEVICES"]="-1"
 
-def train_lstm(dataset, name):
+def train_lstm(dataset, name, model=False):
   # fix random seed for reproducibility
   np.random.seed(7)
   # load the dataset
@@ -25,7 +25,6 @@ def train_lstm(dataset, name):
   # normalize the dataset
   scaler = MinMaxScaler(feature_range=(0, 1))
   dataset = scaler.fit_transform(dataset)
-  pickle.dump( scaler, open( "scaler_{}.p".format(name), "wb" ) )
   # split into train and test sets
   train_size = int(len(dataset) * 0.67)
   test_size = len(dataset) - train_size
@@ -38,10 +37,11 @@ def train_lstm(dataset, name):
   testX = np.reshape(testX, (testX.shape[0], 1, testX.shape[1]))
 
   # create and fit the LSTM network
-  model = Sequential()
-  model.add(LSTM(nfeatures+1, input_shape=(1, nfeatures)))
-  model.add(Dense(nfeatures))
-  model.compile(loss='mean_squared_error', optimizer='adam')
+  if model == False:
+    model = Sequential()
+    model.add(LSTM(nfeatures+1, input_shape=(1, nfeatures)))
+    model.add(Dense(nfeatures))
+    model.compile(loss='mean_squared_error', optimizer='adam')
 
   es = EarlyStopping(monitor='loss', mode='min', verbose=1, patience=5)
   mc = ModelCheckpoint("model_{}.h5".format(name), monitor='loss', mode='min', verbose=1, save_best_only=True)
@@ -61,10 +61,22 @@ def train_lstm(dataset, name):
   print("Train Score: {} RMSE".format(trainScore))
   testScore = math.sqrt(mean_squared_error(testY, testPredict))
   print("Test Score: {} RMSE".format(testScore))
+  return scaler, model
 
 #Uncomment to retrain.
-if True or __name__ == "__main__":
+if __name__ == "__main__":
     df_error = pickle.load( open( 'df_error.p', 'rb'))
     df_state = pickle.load( open( 'df_state.p', 'rb'))
-    train_lstm(df_state.to_numpy(), 'state')
-    train_lstm(df_error.to_numpy(), 'error')
+    model = False
+    scaler = pd.Series(dtype='object')
+    for name, value in df_state.items():
+      s, model = train_lstm(value.to_numpy(), 'state', model)
+      scaler = scaler.append(pd.Series({name: s}))
+    pickle.dump(scaler, open("scaler_state.p", "wb"))
+
+    model = False
+    scaler = pd.Series(dtype='object')
+    for name, value in df_error.items():
+      s, model = train_lstm(value.to_numpy(), 'error', model)
+      scaler = scaler.append(pd.Series({name: s}))
+    pickle.dump(scaler, open("scaler_error.p", "wb"))
