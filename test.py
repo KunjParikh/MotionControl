55b3efd10e1abff402c5b6d87132a658109a8621
Kunj Jagesh Parikh <012532065@coe-hpc1.sjsuad.sjsu.edu>
2020-04-09 11:43:43 -0700

    Need to close plot before next function

--- test.py
+++ test.py
@@ -165,3 +165,4 @@ if True or __name__ == "__main__":
             plt.plot([r[2, 0], r[3, 0]], [r[2, 1], r[3, 1]], 'go-')
         plt.title(function.name)
         plt.savefig("{}.pdf".format(function.name), bbox_inches='tight')
+        plt.close()
