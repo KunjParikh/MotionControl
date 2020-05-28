Restructure training functions Minor fixes. Add scaling in test >> Need to replace it by rolling mean later.

params.py
@@ -17,13 +17,13 @@ class Params:
         # Globals - Function definition
 
         self.functions = [
-            # Function("circle",
-            #          np.vectorize(lambda x, y: (x ** 2) + (y ** 2)),
-            #          lambda x, y: [2 * (x ** 1), 2 * (y ** 1)],
-            #          lambda x, y: [[2, 0], [0, 2]],
-            #          16,
-            #          10
-            #          ),
+            Function("circle",
+                     np.vectorize(lambda x, y: (x ** 2) + (y ** 2)),
+                     lambda x, y: [2 * (x ** 1), 2 * (y ** 1)],
+                     lambda x, y: [[2, 0], [0, 2]],
+                     16,
+                     10
+                     ),
 
             Function("circle_4",
                      np.vectorize(lambda x, y: (x ** 4) + (y ** 4)),
@@ -44,12 +44,10 @@ class Params:
             # np.sqrt can handle array, math.sqrt only works with scalar. > Causes problem with meshgrid and contour
             # plot later.
             Function("elipse",
-                     np.vectorize(lambda x, y: np.sqrt(abs((x ** 2) + (3 * (y ** 2)) + (4 * y) - x - (2 * x * y)))),
-                     lambda x, y: [
-                         ((2 * x) - 1 - (2 * y)) / np.sqrt(abs((x ** 2) + (3 * (y ** 2)) + (4 * y) - x - (2 * x * y))),
-                         ((6 * y) + 4 - (2 * x)) / np.sqrt(abs((x ** 2) + (3 * (y ** 2)) + (4 * y) - x - (2 * x * y)))],
-                     lambda x, y: np.array([[2, 0], [0, 0.5]]),
-                     4,
+                     np.vectorize(lambda x, y: (x ** 2) + (3 * (y ** 2)) + (4 * y) - x - (2 * x * y)),
+                     lambda x, y: [(2 * x) - 1 - (2 * y), (6 * y) + 4 - (2 * x)],
+                     lambda x, y: np.array([[2, -2], [-2, 6]]),
+                     16,
                      10),
 
             Function(
@@ -57,7 +55,8 @@ class Params:
                 np.vectorize(lambda x, y: (x ** 2 + y - 11) ** 2 + (x + y ** 2 - 7) ** 2),
                 lambda x, y: [2 * (x ** 2 + y - 11) * 2 * x + 2 * (x + y ** 2 - 7),
                               2 * (x ** 2 + y - 11) + 2 * (x + y ** 2 - 7) * 2 * y],
-                lambda x, y: np.array([[2, 0], [0, 0.5]]),
+                lambda x, y: np.array([[12 * (x ** 2) + 4 * y - 42, 4 * x + 4 * y],
+                                       [4 * x + 4 * y, 4 * x - 26 + 12 * (y ** 2)]]),
                 144,
                 0.1
             ),
@@ -67,33 +66,21 @@ class Params:
                 np.vectorize(lambda x, y: (x ** 2 + 2 * y - 12) ** 2 + (x + y ** 2 - 17) ** 2),
                 lambda x, y: [(4 * x ** 3 - 46 * x + 8 * x * y + 2 * y ** 2 - 34),
                               (4 * x ** 2 + 4 * x * y - 60 * y + 4 * y ** 3 - 48)],
-                # lambda x, y: np.array([[12 * x ** 2 + 8 * y - 46, 8 * x + 4 * y],
-                #                       [8 * x + 4 * y, 4 * x + 12 * y ** 2 - 60]]),
-                lambda x, y: np.array([[2, 0], [0, 0.5]]),
+                lambda x, y: np.array([[12 * (x ** 2) + 8 * y - 46, 8 * x + 4 * y],
+                                      [8 * x + 4 * y, 4 * x + 12 * (y ** 2) - 60]]),
                 400,
                 0.1
             ),
 
             Function("rhombus",
                      np.vectorize(lambda x, y: abs(x) + abs(y)),
-                     lambda x, y: [np.sign(x), np.sign(y)],
+                     lambda x, y: [np.sign(x), np.sign(y)], #Square cone
                      lambda x, y: [[1, 0], [0, 1]],
                      6,
                      100
                      )
 
         ]
-
-        self.test_functions = [
-            Function("circle",
-                   np.vectorize(lambda x, y: (x ** 2) + (y ** 2)),
-                   lambda x, y: [2 * (x ** 1), 2 * (y ** 1)],
-                   lambda x, y: [[2, 0], [0, 2]],
-                   16,
-                   10
-                   )
-        ]
-        #self.test_functions = self.functions
 
         # Parameters - Formation Control
         self.numSensors = 4

test.py
@@ -28,6 +28,8 @@ if True or __name__ == "__main__":
     df_error = pickle.load(open('df_error.p', 'rb'))
     df_state = pickle.load(open('df_state.p', 'rb'))
 
+
+
     # re-define the batch size
     batch_size = 1
     # re-define model
@@ -66,7 +68,10 @@ if True or __name__ == "__main__":
     print(model_error.summary())
     print(model_hessian.summary())
 
-    for function in params.functions:
+    # testFunctions = [x for x in params.functions if x.name in ['rhombus']]
+    testFunctions = params.functions
+
+    for function in testFunctions:
         r_c, r_c_old, r, = params.r_c, params.r_c, params.r
         x_2, y_2 = 0, 0  # Ignored in formationCenter for i == 0
         q, dq, u_r, vel_q = params.q, params.dq, params.u_r, params.vel_q
@@ -85,8 +90,14 @@ if True or __name__ == "__main__":
 
         keys = list(range(18))
 
+        s_e_kalman_list = []
+        s_e_lstm_list = []
         p_e_kalman_list = []
         p_e_lstm_list = []
+
+        #Rhombus' result improves on rescaling state... a hack.
+        scaler_state.fit(df_state[function.name])
+        scaler_error.fit(df_error[function.name])
 
         stateX = df_state[function.name].iloc[:, 0:3].to_numpy()
 
@@ -134,6 +145,8 @@ if True or __name__ == "__main__":
                 np.hstack((np.zeros(state_predict.shape), state_predict)))[:, 3:]
             s_e_lstm = state_predict[0]
             s_e = s_e_lstm
+            s_e_kalman_list.append(s_e_kalman)
+            s_e_lstm_list.append(s_e_lstm)
 
             p_e_kalman = a @ p @ a.T + m  # 3x3 @ 3x1 @ 3x3 = 3x3
             error = p.flatten()