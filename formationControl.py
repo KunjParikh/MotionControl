365a502209787e063bb2791c8c3aba674219dc8e
Kunj <kunjparikh6@gmail.com>
2020-08-29 12:59:46 -0700

    Use simpler formationControl without any force equation (only q and dq) for both 4 and 2 robot systems.

--- formationControl.py
+++ formationControl.py
@@ -22,20 +22,18 @@ def formationControl(r_c, r, dz_c, q, dq, u_r, vel_q, t):
     y1 = velocity_center
     x1 = p.rotateRight @ y1
 
+    # e_1 = r[1] - r[0]
+    # e_1 = e_1 / np.linalg.norm(e_1)
+    # e_2 = r[2] - r[3]
+    # e_2 = e_2 / np.linalg.norm(e_2)
+    # q_0 = np.array([
+    #     [0, 0],
+    #     (a / sqrt(2)) * e_1,
+    #     (b / sqrt(2)) * e_2,
+    #     [0, 0]
+    # ])
 
     if p.numSensors == 4:
-        e_1 = r[1] - r[0]
-        e_1 = e_1 / np.linalg.norm(e_1)
-        e_2 = r[2] - r[3]
-        e_2 = e_2 / np.linalg.norm(e_2)
-
-        q_0 = np.array([
-            [0, 0],
-            (a / sqrt(2)) * e_1,
-            (b / sqrt(2)) * e_2,
-            [0, 0]
-        ])
-
         q_0 = np.array([
             [0, 0],
             (a / sqrt(2)) * y1,
@@ -43,18 +41,7 @@ def formationControl(r_c, r, dz_c, q, dq, u_r, vel_q, t):
             [0, 0]
         ])
 
-        dq[1:] = dq[1:] + dt * u_r[1:]
-        u_r[1:] = -K2 * (q[1:] - q_0[1:]) - K3 * dq[1:]
-
-        vel_q[1:] = vel_q[1:] + dt * u_r[1:]
-        q[1:] = q[1:] + dt * vel_q[1:]
-
-        q_N = np.append([r_c], q[1:], axis=0)
-
-        r = phi_inv @ q_N
-
     if p.numSensors == 2:
-
         sigma = 10.0
         q_0 = np.array([
             [0, 0],
@@ -67,15 +54,13 @@ def formationControl(r_c, r, dz_c, q, dq, u_r, vel_q, t):
             q_0[1] = array([[sin(sigma), -cos(sigma)],
                             [cos(sigma), sin(sigma)]]) @ q_0[1]
 
-        dq[1:] = dq[1:] + dt * u_r[1:]
-        u_r[1:] = -K2 * (q[1:] - q_0[1:]) - K3 * dq[1:]
-
-        vel_q[1:] = vel_q[1:] + dt * u_r[1:]
-        q[1:] = q[1:] + dt * vel_q[1:]
-
-        q_N = np.append([r_c], q[1:], axis=0)
-
-        r = phi_inv @ q_N
+    # dq[1:] = dq[1:] + dt * u_r[1:]
+    # u_r[1:] = -K2 * (q[1:] - q_0[1:]) - K3 * dq[1:]
+    # vel_q[1:] = vel_q[1:] + dt * u_r[1:]
+    vel_q[1:] = -10 * (q[1:] - q_0[1:])
+    q[1:] = q[1:] + dt * vel_q[1:]
+    q_N = np.append([r_c], q[1:], axis=0)
+    r = phi_inv @ q_N
 
     return r, q, dq, u_r, vel_q
 #
